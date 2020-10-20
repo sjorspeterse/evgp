306dd6be4eaa8a654ee15672dfb8e09ca3d8b768
Sjors Peterse <spsjorspeterse@gmail.com>
2020-10-20 19:38:17 +0200

    now also add the public js

--- public/js/app.js
+++ public/js/app.js
@@ -60057,15 +60057,15 @@ function update_online_counter() {
 
 window.Echo.join('common_room').here(function (users) {
   console.log("Joined!");
-  onlineUsers = users.length * 10;
+  onlineUsers = users.length;
   update_online_counter();
 }).joining(function (user) {
   console.log("Online users: ", onlineUsers);
-  onlineUsers += 10;
+  onlineUsers += 1;
   update_online_counter();
 }).leaving(function (user) {
   console.log("Online users: ", onlineUsers);
-  onlineUsers -= 10;
+  onlineUsers -= 1;
   update_online_counter();
 });
 

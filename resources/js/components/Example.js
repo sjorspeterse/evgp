e72e1d3a037aead655f1c5a98437ef449bcfebb4
Sjors Peterse <spsjorspeterse@gmail.com>
2020-10-20 20:48:23 +0200

    add websockets to react

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
 
 const Example = () => {
+    const [users, setUsers] = useState(0)
+
+    const subscribe = () => {
+        console.log("Subscribing!")
+        window.Echo.join('common_room')
+            .here((users) => {
+                console.log("Joined also in React!")
+                setUsers(users.length)
+            })
+            .joining((user) => {
+                console.log("Online users in React: ", users)
+                setUsers(users => users+ 1)
+            })
+            .leaving((user) => {
+                console.log("Online users in React: ", users)
+                setUsers(users => users - 1)
+            });
+    }
+
+    useEffect(subscribe, [])
+
     return (
         <div className="container">
             <div className="row justify-content-center">
@@ -10,7 +31,7 @@ const Example = () => {
                         <div className="card-header">Example Component</div>
 
                         <div className="card-body">
-                            I'm an example component!
+                            I'm an example component! Nr of users = {users}
                         </div>
                     </div>
                 </div>

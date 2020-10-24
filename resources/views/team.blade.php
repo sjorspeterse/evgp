5fc024495c7db56ad7a4f1938d3d300a716639d7
Sjors Peterse <spsjorspeterse@gmail.com>
2020-10-24 07:24:17 +0200

    Add blade (forgot in previous commit)

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -4,6 +4,7 @@
     <div class="text-white">
         <h1>{{$team->name}}</h1>
         <h2>Canopy</h2>
+        {{-- carSettings link not implemented yet --}}
         {{--<p>{{$team->carSettings->canopy}}</p>--}}
         <h2>Ballast</h2>
         {{-- <p>{{$team->carSettings->ballast}}</p> --}}

b891bb9dd7dc991522ce7951587d5030380ee8fd
Sjors Peterse <spsjorspeterse@gmail.com>
2020-10-31 19:04:05 +0100

    Add newlines in buttons

--- resources/js/components/race/ControlButtons.js
+++ resources/js/components/race/ControlButtons.js
@@ -6,18 +6,18 @@ const ControlButtons = () => {
         <div className="button-wrapper">
             <div className="controlButton stopButton redButton"><span>STOP</span></div>
             <div className="controlButton goButton greenButton"><span>GO</span></div>
-            <div className="controlButton reduceThrottleButton whiteButton"> <span>REDUCE THROTTLE</span> </div>
-            <div className="controlButton increaseThrottleButton whiteButton"> <span>INCREASE THROTTLE</span> </div> 
-            <div className="controlButton doNotPassButton whiteButton"> <span>DO NOT PASS</span> </div> 
-            <div className="controlButton pitLaneButton whiteButton"> <span>GO TO PIT LANE</span> </div> 
-            <div className="controlButton repairFailureButton whiteButton"> <span>REPAIR FAILURE</span> </div> 
-            <div className="controlButton walkingSpeedButton whiteButton"> <span>WALKING SPEED</span> </div> 
-            <div className="controlButton checkSeatbeltButton whiteButton"> <span>CHECK SEATBELT</span> </div> 
-            <div className="controlButton changeBallastButton whiteButton"> <span>CHANGE BALLAST</span> </div> 
-            <div className="controlButton checkHelmetButton whiteButton"> <span>CHECK HELMET</span> </div> 
-            <div className="controlButton checkMirrorsButton whiteButton"> <span>CHECK MIRRORS</span> </div> 
-            <div className="controlButton swapBatteriesButton whiteButton"> <span>SWAP BATTERIES</span> </div> 
-            <div className="controlButton chargeBatteriesButton whiteButton"> <span>CHARGE BATTERIES</span> </div> 
+            <div className="controlButton reduceThrottleButton whiteButton"> <span>REDUCE<br/>THROTTLE</span> </div>
+            <div className="controlButton increaseThrottleButton whiteButton"> <span>INCREASE<br/>THROTTLE</span> </div> 
+            <div className="controlButton doNotPassButton whiteButton"> <span>DO NOT<br/>PASS</span> </div> 
+            <div className="controlButton pitLaneButton whiteButton"> <span>GO TO<br/>PIT LANE</span> </div> 
+            <div className="controlButton repairFailureButton whiteButton"> <span>REPAIR<br/>FAILURE</span> </div> 
+            <div className="controlButton walkingSpeedButton whiteButton"> <span>WALKING<br/>SPEED</span> </div> 
+            <div className="controlButton checkSeatbeltButton whiteButton"> <span>CHECK<br/>SEATBELT</span> </div> 
+            <div className="controlButton changeBallastButton whiteButton"> <span>CHANGE<br/>BALLAST</span> </div> 
+            <div className="controlButton checkHelmetButton whiteButton"> <span>CHECK<br/>HELMET</span> </div> 
+            <div className="controlButton checkMirrorsButton whiteButton"> <span>CHECK<br/>MIRRORS</span> </div> 
+            <div className="controlButton swapBatteriesButton whiteButton"> <span>SWAP <br/>BATTERIES</span> </div> 
+            <div className="controlButton chargeBatteriesButton whiteButton"> <span>CHARGE<br/>BATTERIES</span> </div> 
         </div>
     )
 }

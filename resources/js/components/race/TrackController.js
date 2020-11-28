cd0df28dd5f1be2c69b3810f6dd1ab68926fa333
Sjors Peterse <spsjorspeterse@gmail.com>
2020-11-28 11:33:11 +0100

    Deployment

--- resources/js/components/race/TrackController.js
+++ resources/js/components/race/TrackController.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react"
+import React, {useEffect, useState, useRef} from "react"
 import {usePrevious} from "./CustomHooks"
 import StageSetting from "./StageSetting";
 import {PitLaneActivities, driverChangeActivity, checkMirrorsAcitivity, 

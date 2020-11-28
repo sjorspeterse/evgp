7a9aeafb810c04c4c547de812b1be10d920014e2
Sjors Peterse <spsjorspeterse@gmail.com>
2020-11-28 20:12:31 +0100

    Remove useless outter div

--- resources/js/components/landing/LandingPage.js
+++ resources/js/components/landing/LandingPage.js
@@ -5,61 +5,59 @@ import '../ahmed.css';
 const LandingPage = (props) => {
 
     return (
-        <div style={{"height": "50vh"}}>
-            <div className="container-fluid mainContainer col-12 col-lg-8 col-md-11">
-                <div className="container-fluid E1">
-                    <div className="row mx-0 justify-content-center">	
-                        <div className="col-2 text-center logo1-div text-left pl-0">
-                            <img className=" img-fluid logo" src="/images/logo.jpg"/>
-                        </div>
-                        <div className="col-8 text-center middle-div text-center px-4 py-1 ">
-                            <h3> TEAM PARTICIOATION IN THE VIRTUAL 2020 DCEVGP</h3>
-                            <p > Car No. 185</p>
-                            <p > Your Team Name Goes Here</p>
-                        </div>
-                        <div className="col-2 text-right logo2-div pr-0">
-                            <img className=" img-fluid logo" src="/images/logo_DOEE.png"/>
-                        </div>
+        <div className="container-fluid mainContainer col-12 col-lg-8 col-md-11">
+            <div className="container-fluid E1">
+                <div className="row mx-0 justify-content-center">	
+                    <div className="col-2 text-center logo1-div text-left pl-0">
+                        <img className=" img-fluid logo" src="/images/logo.jpg"/>
+                    </div>
+                    <div className="col-8 text-center middle-div text-center px-4 py-1 ">
+                        <h3> TEAM PARTICIOATION IN THE VIRTUAL 2020 DCEVGP</h3>
+                        <p > Car No. 185</p>
+                        <p > Your Team Name Goes Here</p>
+                    </div>
+                    <div className="col-2 text-right logo2-div pr-0">
+                        <img className=" img-fluid logo" src="/images/logo_DOEE.png"/>
                     </div>
                 </div>
+            </div>
 
-                <div className="container-fluid E2 mt-4 ">
-                    <div className="I1 p-4">
-                        <p className="info px-3">
-                        {'\u00A0\u00A0\u00A0\u00A0\u00A0'} Welcome to the virtual 2020 DC Electric Vehicle Grand Prix. This is a physics-based simulation platform that enables high school teams to compete head-to-head in an exciting vehicle efficiency and strategy competition. Like the annual DC EV Grand Prix held since 2013, teams must follow the rules and regulations to design then race their cars on a challenging track over a period of one hour. <br/> <br/>
-                        {'\u00A0\u00A0\u00A0\u00A0\u00A0'} You will be guided through the four stages of the competition to set up your car design, go through the technical inspection, run a few laps on the track for practice and qualifying, and finally race in two half-hour heats. Just like in the physical race, your objective is to go the maximum number of laps around the track in the allotted time. Team strategy and paying attention to the details of the regulations are essential to winning this competition. 	
-                        </p>
-                        <div className="text-right">
-                            <button 
-                                className="px-2 py-2 btn btn-success " 
-                                onClick={() => props.setPage("race")}
-                            >BEGIN VEHICLE SETUP
-                            </button>
-                        </div>
+            <div className="container-fluid E2 mt-4 ">
+                <div className="I1 p-4">
+                    <p className="info px-3">
+                    {'\u00A0\u00A0\u00A0\u00A0\u00A0'} Welcome to the virtual 2020 DC Electric Vehicle Grand Prix. This is a physics-based simulation platform that enables high school teams to compete head-to-head in an exciting vehicle efficiency and strategy competition. Like the annual DC EV Grand Prix held since 2013, teams must follow the rules and regulations to design then race their cars on a challenging track over a period of one hour. <br/> <br/>
+                    {'\u00A0\u00A0\u00A0\u00A0\u00A0'} You will be guided through the four stages of the competition to set up your car design, go through the technical inspection, run a few laps on the track for practice and qualifying, and finally race in two half-hour heats. Just like in the physical race, your objective is to go the maximum number of laps around the track in the allotted time. Team strategy and paying attention to the details of the regulations are essential to winning this competition. 	
+                    </p>
+                    <div className="text-right">
+                        <button 
+                            className="px-2 py-2 btn btn-success " 
+                            onClick={() => props.setPage("configuration")}
+                        >BEGIN VEHICLE SETUP
+                        </button>
                     </div>
-
-                </div>
-
-                <div className="container-fluid E3 py-4 ">
-                    <div className="I2 p-4 text-center">
-                        <h3>
-                            STAGE OF COMPETITION
-                        </h3>
-                        <table className="table">
-                            <tbody>
-                                <tr>
-                                    <td>VEHICLE SETUP</td>
-                                    <td>TECHNICAL INSPECTION</td>
-                                    <td>PRACTICE & QUALIFYING</td>
-                                    <td>RACE </td>
-                                </tr>
-                            </tbody>
-                        </table>	
-                    </div>
-
                 </div>
 
             </div>
+
+            <div className="container-fluid E3 py-4 ">
+                <div className="I2 p-4 text-center">
+                    <h3>
+                        STAGE OF COMPETITION
+                    </h3>
+                    <table className="table">
+                        <tbody>
+                            <tr>
+                                <td>VEHICLE SETUP</td>
+                                <td>TECHNICAL INSPECTION</td>
+                                <td>PRACTICE & QUALIFYING</td>
+                                <td>RACE </td>
+                            </tr>
+                        </tbody>
+                    </table>	
+                </div>
+
+            </div>
+
         </div>
     )
 }

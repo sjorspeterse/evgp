c01719ec8eff36a04cf8fbd68a04e9d8bebf5ba5
Sjors Peterse <spsjorspeterse@gmail.com>
2020-12-10 17:12:16 +0100

    Deployment

--- public/js/app.js
+++ public/js/app.js
@@ -113215,6 +113215,8 @@ var getInitialRaceLine = function getInitialRaceLine(controlPoints) {
 };
 
 var TrackController = function TrackController(props) {
+  console.log("TrackController: rank = ", props.rank);
+
   var _useState = Object(react__WEBPACK_IMPORTED_MODULE_1__["useState"])(0),
       _useState2 = _slicedToArray(_useState, 2),
       count = _useState2[0],

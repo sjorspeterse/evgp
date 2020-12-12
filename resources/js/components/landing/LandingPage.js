e76280548a180f2a7facd3cd966c792f63acf945
Sjors Peterse <spsjorspeterse@gmail.com>
2020-12-12 09:17:19 +0100

    Allow access to the race only if user1... or official

--- resources/js/components/landing/LandingPage.js
+++ resources/js/components/landing/LandingPage.js
@@ -5,6 +5,17 @@ import '../ahmed.css';
 const LandingPage = (props) => {
 
     const carNr = props.user.carNr
+    const username = props.user.userName
+    let nextButton = <></>
+    if (username.includes('user') || username === 'official') {
+        nextButton = 
+            <button 
+                className="px-2 py-2 btn btn-success " 
+                onClick={() => props.setPage("configuration")}
+                >BEGIN VEHICLE SETUP
+            </button>
+    }
+
     return (
         <div className="container-fluid mainContainer col-12 col-lg-8 col-md-11">
             <div className="container-fluid E1">
@@ -30,11 +41,7 @@ const LandingPage = (props) => {
                     {'\u00A0\u00A0\u00A0\u00A0\u00A0'} You will be guided through the four stages of the competition to set up your car design, go through the technical inspection, run a few laps on the track for practice and qualifying, and finally race in two half-hour heats. Just like in the physical race, your objective is to go the maximum number of laps around the track in the allotted time. Team strategy and paying attention to the details of the regulations are essential to winning this competition. 	
                     </p>
                     <div className="text-right">
-                        <button 
-                            className="px-2 py-2 btn btn-success " 
-                            onClick={() => props.setPage("configuration")}
-                        >BEGIN VEHICLE SETUP
-                        </button>
+                        {nextButton}
                     </div>
                 </div>
 

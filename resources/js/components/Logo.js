5de2d52d797fd4b92e23d45eb215843087aa2720
Sjors Peterse <spsjorspeterse@gmail.com>
2021-08-02 12:04:15 +0200

    update logo

--- resources/js/components/Logo.js
+++ resources/js/components/Logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo = (props) => {
     return (
         // <img src="/images/logo.jpg" className="logo"/> 
-        <img src="/images/combined_logos.png" className="logo"/> 
+        <img src="/images/combined_logos2.png" className="logo"/> 
     );
 }
 
